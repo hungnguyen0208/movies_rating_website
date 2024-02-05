1039735c8b13c2f151d118ce4564314b6207177e
Hung Nguyen <hungnguyen.k51.ftu@gmail.com>
2024-02-06 00:27:27 +0700

    Update components and CSS files

--- client/movie-rating/src/App.js
+++ client/movie-rating/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
 
 function App() {
 
@@ -10,16 +14,28 @@ function App() {
     try {
       const response = await api.get("/api/v1/movies");
 
+      console.log(response.data);
       setMovies(response.data);
-      
+
     } catch (err) {
       console.log(err);
     }
-  return (
-    <div className="App">
-
-    </div>
-  );
 }
+
+useEffect(() => {
+  getMovies();
+},[])
+
+return (
+  <div className="App">
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route path="/" element={<Home movies={movies}/>} ></Route>
+      </Route>
+    </Routes>
+  </div>
+);
+};
 
 export default App;
