e02e8e022c3e13fc2878369e8ddc5b11a90d1964
Hung Nguyen <hungnguyen.k51.ftu@gmail.com>
2024-02-06 00:55:41 +0700

    Added Trailer component using react-player

--- client/movie-rating/src/App.js
+++ client/movie-rating/src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
 
 function App() {
 
@@ -32,6 +33,7 @@ return (
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route path="/" element={<Home movies={movies}/>} ></Route>
+        <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
       </Route>
     </Routes>
   </div>
